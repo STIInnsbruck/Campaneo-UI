56ab5fc8873579856dd539d17de0fc1d41e18156
svencarstenrasmusen <sven.the.eagle@gmail.com>
2021-09-22 22:06:20 +0200

    new web build

--- lib/pages/live_tracker_page.dart
+++ lib/pages/live_tracker_page.dart
@@ -23,6 +23,7 @@ class _LiveTrackerPageState extends State<LiveTrackerPage> {
   void initState() {
     super.initState();
 
+
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
